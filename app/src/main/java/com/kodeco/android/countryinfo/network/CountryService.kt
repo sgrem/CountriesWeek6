6272c98ae5530f8969bff7d1b0abd7b91ad8766c
Steve Gremban <steve.gremban@gmail.com>
2023-10-08 21:23:10 -0500

    Implement cold Flow for CountryService Response

--- app/src/main/java/com/kodeco/android/countryinfo/network/CountryService.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/network/CountryService.kt
@@ -3,8 +3,10 @@ package com.kodeco.android.countryinfo.network
 import com.kodeco.android.countryinfo.models.Country
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface CountryService {
+    @Headers("timeout:5000")
     @GET("all")
     suspend fun getAllCountries(): Response<List<Country>>
 }
